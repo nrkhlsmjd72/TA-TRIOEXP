7b59b55798d5090be454ec4cb5e2fe2a8a89643e
nrkhlsmjd72 <nrkhlsmjd72@gmail.com>
2022-12-05 20:17:30 +0700

    Initial commit

---
+++ app/src/main/java/com/example/makeitso/MakeItSoActivity.kt
@@ -0,0 +1,18 @@
+
+package com.example.makeitso
+
+import android.os.Bundle
+import androidx.activity.compose.setContent
+import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.material.ExperimentalMaterialApi
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+@ExperimentalMaterialApi
+class MakeItSoActivity : AppCompatActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+
+    setContent { MakeItSoApp() }
+  }
+}
